99ef8fcd75fa214162d22ec7e321cead96419e17
AlexRitter1 <ritter.alex@hotmail.com>
2026-05-09 21:11:50 -0300

    refact: add tratamento de espacos na string para descobrir se frases sao palindromos

--- exercicios/palindromo.php
+++ exercicios/palindromo.php
@@ -1,10 +1,13 @@
 <?php
-$palavra = "ana";
+$original = "ana";
 $ehPalindromo = true;
 
 // conseguimos acessar cada caractere através do índice da string
 // echo $palavra[0]; // ana
 // a 
+
+$palavra = str_replace(' ', '', $original);
+$palavra = strtolower($palavra);
 
 for ($i = 0; $i < strlen($palavra) / 2; $i++) {
     // strlen de palavra tem o -1 pra n pegar o índice fora do array no final
@@ -14,5 +17,5 @@ for ($i = 0; $i < strlen($palavra) / 2; $i++) {
     }
 }
 
-echo $ehPalindromo ? "\n$palavra é um palíndromo\n\n" : "\n$palavra não é um palíndromo\n";
+echo $ehPalindromo ? "\n$original é um palíndromo\n\n" : "\n$original não é um palíndromo\n";
 ?>
